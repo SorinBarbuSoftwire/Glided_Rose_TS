1114c3a74e9dafa0a5ffdad9732a0a2e68331d2a
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-06-28 15:41:15 +0300

    refractor for quality increase

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -31,28 +31,25 @@ export class GildedRose {
             else {
                 // Increase
                 if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-
-
-                    if (this.items[i].quality < 50) {
+                    this.items[i].quality = this.items[i].quality + 1;
+                    if (this.items[i].name === 'Aged Brie' && this.items[i].sellIn < 0) {
                         this.items[i].quality = this.items[i].quality + 1;
-                        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                            if (this.items[i].sellIn < 11) {
-                                if (this.items[i].quality < 50) {
-                                    this.items[i].quality = this.items[i].quality + 1
-                                }
-                            }
-                            if (this.items[i].sellIn < 6) {
-                                if (this.items[i].quality < 50) {
-                                    this.items[i].quality = this.items[i].quality + 1
-                                }
-                            }
+                    }
+                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        if (this.items[i].sellIn < 11) {
+                            this.items[i].quality = this.items[i].quality + 1
+                        }
+                        if (this.items[i].sellIn < 6) {
+                            this.items[i].quality = this.items[i].quality + 1
                         }
                     }
-
-
                 }
                 // Keep
             }
+            if (this.items[i].quality > 50) {
+                this.items[i].quality = 50;
+            }
+
             /* SellIn */
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
@@ -67,10 +64,6 @@ export class GildedRose {
                         }
                     } else {
                         this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
                     }
                 }
             }
